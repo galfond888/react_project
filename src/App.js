76c6e43d52b97e02a586487d26d31a7fd16564b1
gal <tanjhu@163.com>
2020-06-30 21:24:40 +0800

    首次傳遞props成功

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React  from 'react'
+
+class Memo extends React.Component {
+  render() {
+    return (
+      <div >
+        <h1>厲害啦 List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default Memo
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React  from 'react';
+import ReactDOM from 'react-dom';
+import Memo from './App'
+import './index.css';
+
+
+ReactDOM.render(<Memo name="Gal The best programmer" />,document.getElementById('root'))
+
